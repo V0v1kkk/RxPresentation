540a56b5a05a608a0dee00e3f9ef7f5ad0babadb
Vladimir Rogozhin <vladimirrogozhin90@gmail.com>
2019-11-13 23:55:35 +0300

    Add project files.

---
+++ RxMulticastingDemo/ObservableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RxMulticastingDemo
+{
+    public static class ObservableExtensions
+    {
+        private static readonly object Lock = new object();
+
+        public static IDisposable SubscribeConsole<T>(this IObservable<T> observable, int number, ConsoleColor color)
+        {
+            return observable.Subscribe(obj =>
+            {
+                lock (Lock)
+                {
+                    var originalColor = Console.ForegroundColor;
+                    Console.ForegroundColor = color;
+                    Console.WriteLine($"Subscriber {number} - {obj}");
+                    Console.ForegroundColor = originalColor;
+                }
+            });
+        }
+    }
+}
---
+++ RxMulticastingDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace RxMulticastingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+
+            var random = new Random();
+
+
+            var observable = Observable.Interval(TimeSpan.FromSeconds(2))
+                .Select(_ => random.Next(1, 10))
+                .Take(5);
+
+            observable.SubscribeConsole(1, ConsoleColor.DarkCyan); // subscriber 1
+            observable.SubscribeConsole(2, ConsoleColor.DarkRed);  // subscriber 2
+
+
+            var connectableObservable = observable.Publish();
+
+            connectableObservable.SubscribeConsole(3, ConsoleColor.Black); // subscriber 3
+            connectableObservable.SubscribeConsole(4, ConsoleColor.Blue);  // subscriber 4
+
+            connectableObservable.Connect();
+
+
+            observable.Wait();
+        }
+    }
+}
