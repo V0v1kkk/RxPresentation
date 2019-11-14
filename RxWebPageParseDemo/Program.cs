6fc61020c851c2f9ca4168cb25dad6106344d6a5
Vladimir Rogozhin <vladimirrogozhin90@gmail.com>
2019-11-14 12:58:00 +0300

    RxWebPageParseDemo project added. ObservableFromFlowInterval added into RxWebPageParseDemo.

---
+++ RxWebPageParseDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace RxWebPageParseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        IObservable<Unit> ObservableFromFlowInterval(TimeSpan period, TimeSpan notSmaller, TimeSpan maxOtklon)
+        {
+            var random = new Random();
+            long LongRandom(long min, long max, Random rand)
+            {
+                byte[] buf = new byte[8];
+                rand.NextBytes(buf);
+                long longRand = BitConverter.ToInt64(buf, 0);
+                return (Math.Abs(longRand % (max - min)) + min);
+            }
+
+            TimeSpan GetNextInterval(long lastTicks)
+            {
+                var narmalizeTicks = lastTicks + notSmaller.Ticks;
+                var maxPlus = LongRandom(narmalizeTicks, maxOtklon.Ticks, random) - lastTicks;
+                return TimeSpan.FromTicks(maxPlus);
+            }
+
+            return Observable.Generate(DateTimeOffset.Now.Ticks,
+                s => true,
+                s => s,
+                s => s,
+                GetNextInterval)
+                .Select(_ => Unit.Default);
+        }
+
+    }
+}
