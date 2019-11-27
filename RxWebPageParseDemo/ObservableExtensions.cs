a54ea1a29622cf33ed7395e9721863e246d728e2
Vladimir Rogozhin <vladimirrogozhin90@gmail.com>
2019-11-27 09:33:31 +0300

    GetWebPageText extension now makes requests similarly to browser.

--- RxWebPageParseDemo/ObservableExtensions.cs
+++ RxWebPageParseDemo/ObservableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Reactive;
@@ -16,9 +18,24 @@ namespace RxWebPageParseDemo
             {
                 using (var client = new HttpClient())
                 {
-                    var response = await client.GetAsync(pageUri);
-                    var pageContents = await response.Content.ReadAsStringAsync();
-                    return pageContents;
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, pageUri))
+                    {
+                        request.Headers.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml");
+                        request.Headers.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+                        request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+                        request.Headers.TryAddWithoutValidation("Accept-Charset", "ISO-8859-1");
+
+                        using (var response = await client.SendAsync(request).ConfigureAwait(false))
+                        {
+                            response.EnsureSuccessStatusCode();
+                            using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                            using (var decompressedStream = new GZipStream(responseStream, CompressionMode.Decompress))
+                            using (var streamReader = new StreamReader(decompressedStream))
+                            {
+                                return await streamReader.ReadToEndAsync().ConfigureAwait(false);
+                            }
+                        }
+                    }
                 }
             }));
         }
